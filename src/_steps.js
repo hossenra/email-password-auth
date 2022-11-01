a59e5961cbd5ca7afdb8eaa91c2720404dab5f49
RAZIBUL HOSSEN <razibulhossen@MacBook-Air-3.local>
2022-11-01 08:49:19 +0100

    first commit

---
+++ src/_steps.js
@@ -0,0 +1,14 @@
+/* 
+Auth Terminology: 
+-----------------------
+"Register" or "Sign up" [First time create a user]
+"log in" or  "sigh in" [Already you have a user account created]
+
+*/
+
+/* 
+  get form value:
+  set form onSubmitHandler
+  set event parameter
+  event.target.email.value 
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD__7DhMxauguvQgBQT8Dh0_gu2RK_NYEU",
+  authDomain: "email-password-auth-803ef.firebaseapp.com",
+  projectId: "email-password-auth-803ef",
+  storageBucket: "email-password-auth-803ef.appspot.com",
+  messagingSenderId: "641628648726",
+  appId: "1:641628648726:web:8774ae9432a25257764c37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
